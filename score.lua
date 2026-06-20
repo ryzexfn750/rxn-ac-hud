9d8cb35b5e313883a3597b790bc90bb573f08c1c
Ryze <ninettino150@gmail.com>
2026-06-20 23:56:50 +0200

    Update score.lua

--- score.lua
+++ score.lua
@@ -158,7 +158,7 @@ function script.drawUI()
 
   ui.drawLine(p + vec2(20, 48), p + vec2(292, 48), line, 1)
 
-  ui.drawImage(logoUrl, p + vec2(18, 13), p + vec2(69, 26))
+  ui.drawImage(logoUrl, p + vec2(18, 13), p + vec2(69, 66))
   ui.drawLine(p + vec2(81, 10), p + vec2(81, 32), separator, 1.5)
 
   ui.setCursor(p + vec2(92, 12))
